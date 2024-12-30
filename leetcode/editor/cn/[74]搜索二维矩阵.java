85eb9b0159195b8f2faa48317c0f90ca790f44e3
TTT <1794230131@qq.com>
2024-12-30 21:46:10 +0800

    2024.12.30

---
+++ leetcode/editor/cn/[74]搜索二维矩阵.java
@@ -0,0 +1,80 @@
+//给你一个满足下述两条属性的 m x n 整数矩阵： 
+//
+// 
+// 每行中的整数从左到右按非严格递增顺序排列。 
+// 每行的第一个整数大于前一行的最后一个整数。 
+// 
+//
+// 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 100 
+// -10⁴ <= matrix[i][j], target <= 10⁴ 
+// 
+//
+// Related Topics 数组 二分查找 矩阵 👍 1000 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length, n = matrix[0].length;
+
+        // 首先在第一列使用二分查找确定目标可能所在的行
+        int top = 0, bottom = m - 1;
+        while (top <= bottom) {
+            int mid = top + (bottom - top) / 2;
+            if (matrix[mid][0] == target) {
+                return true;
+            } else if (matrix[mid][0] < target) {
+                top = mid + 1;
+            } else {
+                bottom = mid - 1;
+            }
+        }
+
+        // 确定目标所在行
+        int row = bottom;
+        if (row < 0) {
+            return false;
+        }
+
+        // 在确定的行中使用二分查找
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
